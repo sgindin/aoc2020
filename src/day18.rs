3cc3cf26cc51b814be03393fb32bfa44d1401984
sgindin <sergey.gindin@gmail.com>
2021-01-04 16:52:32 +0100

    Day 17. Day 18 (part 2).

--- src/day18.rs
+++ src/day18.rs
@@ -1,6 +1,6 @@
 use crate::tools;
 
-fn eval_ops(mut ops: Vec<char>, mut vals: Vec<i64>) -> i64 {
+fn eval_postfix(mut ops: Vec<char>, mut vals: Vec<i64>) -> i64 {
     while !ops.is_empty() {
         let op = ops.pop().unwrap();
         let left = vals.pop().unwrap();
@@ -16,7 +16,32 @@ fn eval_ops(mut ops: Vec<char>, mut vals: Vec<i64>) -> i64 {
     *vals.first().unwrap()
 }
 
-fn evaluate(expression: &str) -> i64 {
+fn unwind(ops: &mut Vec<char>, vals: &mut Vec<i64>, add_over_mul: bool) {
+    let mut o = Vec::new();
+    let mut v = Vec::new();
+    while !ops.is_empty() {
+        let op = ops.pop().unwrap();
+        match op {
+            '(' => break,
+            '+' if add_over_mul => {
+                let left = vals.pop().unwrap();
+                let right = vals.pop().unwrap();
+                vals.push(left + right);
+            },
+            '+'|'*' => {
+                o.push(op);
+                v.push(vals.pop().unwrap());
+            },
+            _ => unreachable!(),
+        }
+    }
+    if !o.is_empty() {
+        v.push(vals.pop().unwrap());
+        vals.push(eval_postfix(o, v));
+    }
+}
+
+fn evaluate(expression: &str, add_over_mul: bool) -> i64 {
     let mut ops = Vec::new();
     let mut vals = Vec::new();
     let mut val = String::default();
@@ -24,34 +49,21 @@ fn evaluate(expression: &str) -> i64 {
     for c in expression.chars().chain(std::iter::once(' ')) {
         if c.is_digit(10) {
             val.push(c);
-        } else {
-            if !val.is_empty() {
-                vals.push(val.parse::<i64>().unwrap());
-                val.clear();
-            }
-            match c {
-                '+' | '*' | '(' => ops.push(c),
-                ')' => {
-                    let mut o = Vec::new();
-                    let mut v = Vec::new();
-                    while *ops.last().unwrap() != '(' {
-                        o.push(ops.pop().unwrap());
-                        v.push(vals.pop().unwrap());
-                    }
-                    v.push(vals.pop().unwrap());
-                    vals.push(eval_ops(o, v));
-                    assert_eq!(ops.pop().unwrap(), '(');
-                },
-                ' ' => (),
-                _ => unreachable!(),
-            }
+            continue
+        }
+        if !val.is_empty() {
+            vals.push(val.parse::<i64>().unwrap());
+            val.clear();
+        }
+        match c {
+            '*'|'+'|'(' => ops.push(c),
+            ')' => unwind(&mut ops, &mut vals, add_over_mul),
+            ' ' => (),
+            _ => unreachable!(),
         }
     }
-
-    ops.reverse();
-    vals.reverse();
-
-    eval_ops(ops, vals)
+    unwind(&mut ops, &mut vals, add_over_mul);
+    *vals.first().unwrap()
 }
 
 #[cfg(test)]
@@ -60,35 +72,67 @@ mod tests {
 
     #[test]
     fn test_simple() {
-        assert_eq!(evaluate("1 + 2 * 3"), 9);
+        assert_eq!(evaluate("1 + 2 * 3", false), 9);
     }
 
     #[test]
+    fn test_simple_2() { assert_eq!(evaluate("1 + 2 * 3", true), 9); }
+
+    #[test]
     fn test_braces_beg() {
-        assert_eq!(evaluate("(1 + 2) * 3"), 9);
+        assert_eq!(evaluate("(1 + 2) * 3", false), 9);
+    }
+
+    #[test]
+    fn test_braces_beg_2() {
+        assert_eq!(evaluate("(1 + 2) * 3", true), 9);
     }
 
     #[test]
     fn test_braces_end() {
-        assert_eq!(evaluate("1 + (2 * 3)"), 7);
+        assert_eq!(evaluate("1 + (2 * 3)", false), 7);
+    }
+
+    #[test]
+    fn test_braces_end_2() {
+        assert_eq!(evaluate("1 + (2 * 3)", true), 7);
     }
 
     #[test]
     fn test_braces_mid() {
-        assert_eq!(evaluate("1 + (2 * 3) + 4"), 11);
+        assert_eq!(evaluate("1 + (2 * 3) + 4", false), 11);
+    }
+
+    #[test]
+    fn test_braces_mid_2() {
+        assert_eq!(evaluate("1 + (2 * 3) + 4", true), 11);
     }
 
     #[test]
     fn test_braces_nested() {
-        assert_eq!(evaluate("1 + (2 + (3 * 4) * 2)"), 29);
+        assert_eq!(evaluate("1 + (2 + (3 * 4) * 2)", false), 29);
+    }
+
+    #[test]
+    fn test_braces_nested_2() { assert_eq!(evaluate("1 + (2 + (3 * 4) * 2)", true), 29); }
+
+    #[test]
+    fn test_aoc_2() {
+        assert_eq!(evaluate("1 + (2 * 3) + (4 * (5 + 6))", true), 51);
+        assert_eq!(evaluate("2 * 3 + (4 * 5)", true), 46);
+        assert_eq!(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)", true), 1445);
+        assert_eq!(evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", true), 669060);
+        assert_eq!(evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", true), 23340);
     }
 }
 
 pub fn solve() {
-    let part1 = tools::read_lines("./input/day18.txt")
+    let (part1, part2) = tools::read_lines("./input/day18.txt")
         .unwrap()
-        .fold(0, |acc, line| {
-            acc + evaluate(&line.unwrap())
+        .fold((0, 0), |(part1, part2), line| {
+            let line = line.unwrap();
+            (part1 + evaluate(&line, false),
+                part2 + evaluate(&line, true))
         });
-    println!("{}", part1)
+    println!("{}, {}", part1, part2)
 }
